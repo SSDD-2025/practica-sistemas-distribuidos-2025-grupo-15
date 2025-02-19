e6343a7340d98448d471908ebdc4b22e1a186e25
RubenCamach0 <r.camacho.2022@alumnos.urjc.es>
2025-02-19 22:49:32 +0100

    OrderService

--- demo/src/main/java/com/example/demo/Order/OrderRepository.java
+++ demo/src/main/java/com/example/demo/Order/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.Order;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     
+    Collection<Order> findAllByOrderUser();
 }
