bc7e807ebdade0f90ef8b0e70516e6fd4f73200f
Luciagalan38 <l.galang.2019@alumnos.urjc.es>
2025-02-20 09:17:10 +0100

    Book Service

--- demo/src/main/java/com/example/demo/Book/BookRepository.java
+++ demo/src/main/java/com/example/demo/Book/BookRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.Book;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByAuthor (String author );
+    List<Book> findByTitle (String title );
 }
---
+++ demo/src/main/java/com/example/demo/Book/BookService.java
@@ -0,0 +1,54 @@
+package com.example.demo.Book;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    BookRepository bookRepository;
+
+    public Collection<Book>getBooks(){
+        Iterable<Book> books = bookRepository.findAll();
+        Collection<Book> bookList = new ArrayList<>();
+        books.forEach(bookList::add);
+        return bookList;
+    }
+
+    public Book getBook(int ISBN){     
+        return bookRepository.findById(ISBN).get();
+    }
+
+    public Book getBook(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+    public List<Book> getBooks(String author) {
+        return bookRepository.findByAuthor(author);
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public boolean updateBook(Book book) {
+        if (bookRepository.existsById(book.getISBN())) {
+            bookRepository.save(book);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteBook(int ISBN) {
+        if (bookRepository.existsById(ISBN)) {
+            bookRepository.deleteById(ISBN);
+            return true;
+        }
+        return false;
+    }
+}
+
